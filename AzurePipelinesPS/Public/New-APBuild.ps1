5decd8cf2d4aa22f1cc200461b5f0ed148824afe
Michael DeJulia <Michael.DeJulia@PacificLife.com>
2018-10-17 13:28:52 -0700

    New-APBuild support

---
+++ AzurePipelinesPS/Public/New-APBuild.ps1
@@ -0,0 +1,163 @@
+function New-APBuild
+{
+    <#
+    .SYNOPSIS
+
+    Creates an Azure Pipeline build.
+
+    .DESCRIPTION
+
+    Creates an Azure Pipeline build by definition id.
+    The id can be retrieved by using Get-APbuildList.
+
+  .PARAMETER Instance
+    
+    The Team Services account or TFS server.
+    
+    .PARAMETER Collection
+    
+    For Azure DevOps the value for collection should be the name of your orginization. 
+    For both Team Services and TFS The value should be DefaultCollection unless another collection has been created.
+
+    .PARAMETER Project
+    
+    Project ID or project name.
+
+    .PARAMETER ApiVersion
+    
+    Version of the api to use.
+
+    .PARAMETER PersonalAccessToken
+    
+    Personal access token used to authenticate that has been converted to a secure string. 
+    It is recomended to uses an Azure Pipelines PS session to pass the personal access token parameter among funcitons, See New-APSession.
+    https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts
+
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to send the request.
+
+    .PARAMETER Session
+
+    Azure DevOps PS session, created by New-APSession.
+
+    .PARAMETER Name
+
+    The name of the build definition to queue.
+
+    .INPUTS
+    
+
+    .OUTPUTS
+
+    PSObject, Azure Pipelines variable group.
+
+    .EXAMPLE
+
+    .LINK
+
+    https://docs.microsoft.com/en-us/rest/api/vsts/distributedtask/variablegroups/add?view=vsts-rest-5.0
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByPersonalAccessToken')]
+    Param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Instance,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Collection,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Project,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $ApiVersion,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Security.SecureString]
+        $PersonalAccessToken,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter()]
+        [string]
+        $Name 
+    )
+
+    begin
+    {
+        If ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-APSession
+            If ($currentSession)
+            {
+                $Instance = $currentSession.Instance
+                $Collection = $currentSession.Collection
+                $Project = $currentSession.Project
+                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                $PersonalAccessToken = $currentSession.PersonalAccessToken
+            }
+        }
+    }
+        
+    process
+    {
+        $definition = Get-APBuildDefinitionList -Instance $Instance -Collection $Collection -Project $Project -ApiVersion $ApiVersion -Name $Name
+        $body = @{
+            definition = $definition
+        }
+        $apiEndpoint = Get-APApiEndpoint -ApiType 'build-builds'
+        $setAPUriSplat = @{
+            Collection  = $Collection
+            Instance    = $Instance
+            Project     = $Project
+            ApiVersion  = $ApiVersion
+            ApiEndpoint = $apiEndpoint
+        }
+        [uri] $uri = Set-APUri @setAPUriSplat
+        $invokeAPRestMethodSplat = @{
+            Method              = 'POST'
+            Uri                 = $uri
+            Credential          = $Credential
+            PersonalAccessToken = $PersonalAccessToken
+            Body                = $body
+            ContentType         = 'application/json'
+        }
+        $results = Invoke-APRestMethod @invokeAPRestMethodSplat 
+        If ($results.value)
+        {
+            return $results.value
+        }
+        else
+        {
+            return $results
+        }
+    }
+    
+    end
+    {
+    }
+}
