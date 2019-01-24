24571534489a52790f5068e738479c99c8d1edb2
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 11:57:35 -0800

    Get-APBuild

--- AzurePipelinesPS/Public/Get-APBuild.ps1
+++ AzurePipelinesPS/Public/Get-APBuild.ps1
@@ -130,8 +130,15 @@ function Get-APBuild
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
                 $Project = $currentSession.Project
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
---
+++ AzurePipelinesPS/Tests/Get-APBuild.Tests.ps1
@@ -0,0 +1,45 @@
+$Script:ModuleName = 'AzurePipelinesPS'
+$Script:ModuleRoot = Split-Path -Path $PSScriptRoot -Parent
+$Script:ModuleManifestPath = "$ModuleRoot\..\Output\$ModuleName\$ModuleName.psd1"
+$Script:TestDataPath = "TestDrive:\ModuleData.json"
+Import-Module $ModuleManifestPath -Force
+InModuleScope $ModuleName {
+    #region testParams
+    $Function = 'Get-APBuild'
+    $newApSessionSplat = @{
+        Collection          = 'myCollection'
+        Project             = 'myProject'
+        Instance            = 'https://dev.azure.com/'
+        PersonalAccessToken = 'myToken'
+        ApiVersion          = '5.0-preview'
+        SessionName         = 'ADOmyProject'
+    }
+    $session = New-APSession @newApSessionSplat
+    $_buildId = 7
+    $_uri = ('https://dev.azure.com/myCollection/myProject/_apis/build/builds/{0}/?api-version=5.0-preview' -f $_buildId)
+    $_apiEndpoint = 'build-buildId'
+    #endregion testParams
+
+    Describe "Function: [$Function]" {   
+        Mock -CommandName Get-APApiEndpoint -ParameterFilter { $ApiType -eq $_apiEndpoint } -MockWith {
+            Return $_apiEndpoint
+        }
+        Mock -CommandName Set-APUri -MockWith {
+            Return $_uri
+        }
+        Context 'Session' {
+            Mock -CommandName Invoke-APRestMethod -ParameterFilter { $Uri.AbsoluteUri -eq $_uri } -MockWith {
+                Return 'Mocked Invoke-APRestMethod'
+            }
+            It 'should accept session' {
+                Get-APBuild -Session $session -BuildId $_buildId | Should be 'Mocked Invoke-APRestMethod'
+                Assert-MockCalled -CommandName 'Get-APApiEndpoint' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Set-APUri' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Invoke-APRestMethod' -Times 1 -Exactly
+            }
+        }
+    }
+    $session | Remove-APSession
+}
+
+
