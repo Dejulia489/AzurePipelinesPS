298cda2987dbf3c0d1cd1a34cf0c9dcc409b9dcb
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 13:33:15 -0800

    Get-APReleaseDefinition

--- AzurePipelinesPS/Public/Get-APReleaseDefinition.ps1
+++ AzurePipelinesPS/Public/Get-APReleaseDefinition.ps1
@@ -131,8 +131,15 @@ function Get-APReleaseDefinition
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
                 $Project = $currentSession.Project
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
