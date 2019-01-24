92caea0ee36a0219b6e1fe8d39eceaba6fe0ef97
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 12:25:04 -0800

    Get-APTargetList

---
+++ AzurePipelinesPS/Tests/Get-APTargetList.Tests.ps1
@@ -0,0 +1,45 @@
+$Script:ModuleName = 'AzurePipelinesPS'
+$Script:ModuleRoot = Split-Path -Path $PSScriptRoot -Parent
+$Script:ModuleManifestPath = "$ModuleRoot\..\Output\$ModuleName\$ModuleName.psd1"
+$Script:TestDataPath = "TestDrive:\ModuleData.json"
+Import-Module $ModuleManifestPath -Force
+InModuleScope $ModuleName {
+    #region testParams
+    $Function = 'Get-APTargetList'
+    $newApSessionSplat = @{
+        Collection          = 'myCollection'
+        Project             = 'myProject'
+        Instance            = 'https://dev.azure.com/'
+        PersonalAccessToken = 'myToken'
+        ApiVersion          = '5.0-preview'
+        SessionName         = 'ADOmyProject'
+    }
+    $session = New-APSession @newApSessionSplat
+    $_deploymentGroupId = 7
+    $_uri = ('https://dev.azure.com/myCollection/myProject/_apis/distributedtask/deploymentgroups/{0}/targets/?api-version=5.0-preview' -f $_deploymentGroupId)
+    $_apiEndpoint = 'distributedtask-targets'
+    #endregion testParams
+
+    Describe "Function: [$Function]" -Tag 'Pending' {   
+        Mock -CommandName Get-APApiEndpoint -ParameterFilter { $ApiType -eq $_apiEndpoint } -MockWith {
+            Return $_apiEndpoint
+        }
+        Mock -CommandName Set-APUri -MockWith {
+            Return $_uri
+        }
+        Context 'Session' {
+            Mock -CommandName Invoke-APRestMethod -ParameterFilter { $Uri.AbsoluteUri -eq $_uri } -MockWith {
+                Return 'Mocked Invoke-APRestMethod'
+            }
+            It 'should accept session' {
+                Get-APTargetList -Session $session -DeploymentGroupID $_deploymentGroupId | Should be 'Mocked Invoke-APRestMethod'
+                Assert-MockCalled -CommandName 'Get-APApiEndpoint' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Set-APUri' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Invoke-APRestMethod' -Times 1 -Exactly
+            }
+        }
+    }
+    $session | Remove-APSession
+}
+
+
