cc99bc64465574a074bc38b18096e7de28877fb5
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 13:20:32 -0800

    Get-APFeed

--- AzurePipelinesPS/Public/Get-APFeed.ps1
+++ AzurePipelinesPS/Public/Get-APFeed.ps1
@@ -119,8 +119,16 @@ function Get-APFeed
             {
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                $Project = $currentSession.Project
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
