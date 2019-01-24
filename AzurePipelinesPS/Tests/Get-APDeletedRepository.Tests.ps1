a84c9e5eae69bd6dc4e85340ef213e81a5505c3b
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 13:17:48 -0800

    Get-APDeletedRepository

---
+++ AzurePipelinesPS/Tests/Get-APDeletedRepository.Tests.ps1
@@ -0,0 +1,44 @@
+$Script:ModuleName = 'AzurePipelinesPS'
+$Script:ModuleRoot = Split-Path -Path $PSScriptRoot -Parent
+$Script:ModuleManifestPath = "$ModuleRoot\..\Output\$ModuleName\$ModuleName.psd1"
+$Script:TestDataPath = "TestDrive:\ModuleData.json"
+Import-Module $ModuleManifestPath -Force
+InModuleScope $ModuleName {
+    #region testParams
+    $Function = 'Get-APDeletedRepository'
+    $newApSessionSplat = @{
+        Collection          = 'myCollection'
+        Project             = 'myProject'
+        Instance            = 'https://dev.azure.com/'
+        PersonalAccessToken = 'myToken'
+        ApiVersion          = '5.0-preview'
+        SessionName         = 'ADOmyProject'
+    }
+    $session = New-APSession @newApSessionSplat
+    $_uri = 'https://dev.azure.com/myCollection/myProject/_apis/git/deletedrepositories?api-version=5.0-preview'
+    $_apiEndpoint = 'git-deletedrepositories'
+    #endregion testParams
+
+    Describe "Function: [$Function]" {   
+        Mock -CommandName Get-APApiEndpoint -ParameterFilter { $ApiType -eq $_apiEndpoint } -MockWith {
+            Return $_apiEndpoint
+        }
+        Mock -CommandName Set-APUri -MockWith {
+            Return $_uri
+        }
+        Context 'Session' {
+            Mock -CommandName Invoke-APRestMethod -ParameterFilter { $Uri.AbsoluteUri -eq $_uri } -MockWith {
+                Return 'Mocked Invoke-APRestMethod'
+            }
+            It 'should accept session' {
+                Get-APDeletedRepository -Session $session | Should be 'Mocked Invoke-APRestMethod'
+                Assert-MockCalled -CommandName 'Get-APApiEndpoint' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Set-APUri' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Invoke-APRestMethod' -Times 1 -Exactly
+            }
+        }
+    }
+    $session | Remove-APSession
+}
+
+
