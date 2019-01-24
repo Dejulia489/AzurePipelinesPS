fa870aeb4f2f943f9c23c1b9ece2be052dac23e1
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 12:09:43 -0800

    Get-APPackageList

--- AzurePipelinesPS/Public/Get-APPackageList.ps1
+++ AzurePipelinesPS/Public/Get-APPackageList.ps1
@@ -221,8 +221,16 @@ function Get-APPackageList
             {
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                $Project = $currentSession.Project
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
