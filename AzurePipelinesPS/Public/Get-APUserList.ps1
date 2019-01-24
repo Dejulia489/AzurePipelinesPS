52ea4d064b8b8e8d0ecfe09f1843f7c23ed34ec2
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 12:29:28 -0800

    Get-APUserList

--- AzurePipelinesPS/Public/Get-APUserList.ps1
+++ AzurePipelinesPS/Public/Get-APUserList.ps1
@@ -121,8 +121,16 @@ function Get-APUserList
             {
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                $Project = $currentSession.Project
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
