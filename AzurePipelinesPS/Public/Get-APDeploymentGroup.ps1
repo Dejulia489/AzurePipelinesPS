bf909f242b119a78d6fc4386aee3adb74b9385b2
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 13:19:17 -0800

    Get-APDeploymentGroup

--- AzurePipelinesPS/Public/Get-APDeploymentGroup.ps1
+++ AzurePipelinesPS/Public/Get-APDeploymentGroup.ps1
@@ -140,8 +140,15 @@ function Get-APDeploymentGroup
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
                 $Project = $currentSession.Project
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
