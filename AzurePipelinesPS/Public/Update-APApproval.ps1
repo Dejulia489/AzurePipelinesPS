20829e1c9c1c912f32956f8b73c8cc903bf6d74b
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 15:01:46 -0800

    Update-APApproval

--- AzurePipelinesPS/Public/Update-APApproval.ps1
+++ AzurePipelinesPS/Public/Update-APApproval.ps1
@@ -139,8 +139,15 @@ function Update-ApApproval
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
                 $Project = $currentSession.Project
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
---
+++ AzurePipelinesPS/Tests/Update-APApproval.Tests.ps1
@@ -0,0 +1,46 @@
+$Script:ModuleName = 'AzurePipelinesPS'
+$Script:ModuleRoot = Split-Path -Path $PSScriptRoot -Parent
+$Script:ModuleManifestPath = "$ModuleRoot\..\Output\$ModuleName\$ModuleName.psd1"
+$Script:TestDataPath = "TestDrive:\ModuleData.json"
+Import-Module $ModuleManifestPath -Force
+InModuleScope $ModuleName {
+    #region testParams
+    $Function = 'Update-APApproval'
+    $newApSessionSplat = @{
+        Collection          = 'myCollection'
+        Project             = 'myProject'
+        Instance            = 'https://dev.azure.com/'
+        PersonalAccessToken = 'myToken'
+        ApiVersion          = '5.0-preview'
+        SessionName         = 'ADOmyProject'
+    }
+    $session = New-APSession @newApSessionSplat
+    $_approvalId = 7
+    $_status = 'approved'
+    $_uri = ('https://dev.azure.com/myCollection/myProject/_apis/release/approvals/{0}?api-version=5.0-preview' -f $_approvalId)
+    $_apiEndpoint = 'release-approvalId'
+    #endregion testParams
+
+    Describe "Function: [$Function]" {   
+        Mock -CommandName Get-APApiEndpoint -ParameterFilter { $ApiType -eq $_apiEndpoint } -MockWith {
+            Return $_apiEndpoint
+        }
+        Mock -CommandName Set-APUri -MockWith {
+            Return $_uri
+        }
+        Context 'Session' {
+            Mock -CommandName Invoke-APRestMethod -ParameterFilter { $Uri.AbsoluteUri -eq $_uri } -MockWith {
+                Return 'Mocked Invoke-APRestMethod'
+            }
+            It 'should accept session' {
+                Update-APApproval -Session $session -ApprovalId $_approvalId -Status $_status | Should be 'Mocked Invoke-APRestMethod'
+                Assert-MockCalled -CommandName 'Get-APApiEndpoint' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Set-APUri' -Times 1 -Exactly
+                Assert-MockCalled -CommandName 'Invoke-APRestMethod' -Times 1 -Exactly
+            }
+        }
+    }
+    $session | Remove-APSession
+}
+
+
