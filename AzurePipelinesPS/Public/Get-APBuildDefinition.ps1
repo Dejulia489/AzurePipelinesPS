16e69947c9ddf3386bb4fd206b820191b86a4814
mdejulia <Michael.DeJulia@PacificLife.com>
2019-01-24 11:59:29 -0800

    Get-APBuildDefinition

--- AzurePipelinesPS/Public/Get-APBuildDefinition.ps1
+++ AzurePipelinesPS/Public/Get-APBuildDefinition.ps1
@@ -154,8 +154,15 @@ function Get-APBuildDefinition
                 $Instance = $currentSession.Instance
                 $Collection = $currentSession.Collection
                 $Project = $currentSession.Project
-                $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
                 $PersonalAccessToken = $currentSession.PersonalAccessToken
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
             }
         }
     }
