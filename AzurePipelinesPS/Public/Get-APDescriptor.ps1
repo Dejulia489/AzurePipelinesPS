9e6e73b71ed7dd4fc2d330059ac9bd4c50dcf468
TStoian <tstoian@munichre.com>
2020-04-10 17:20:57 +0300

    Added Get-APDescriptor function

---
+++ AzurePipelinesPS/Public/Get-APDescriptor.ps1
@@ -0,0 +1,184 @@
+function Get-APDescriptor
+{
+    <#
+    .SYNOPSIS
+
+    Resolve a storage key to a descriptor.
+
+    .DESCRIPTION
+
+    Resolve a storage key to a descriptor.
+
+    .PARAMETER Instance
+    
+    The Team Services account or TFS server.
+    
+    .PARAMETER Collection
+    
+    For Azure DevOps the value for collection should be the name of your orginization. 
+    For both Team Services and TFS The value should be DefaultCollection unless another collection has been created.
+
+    .PARAMETER ApiVersion
+    
+    Version of the api to use.
+
+    .PARAMETER PersonalAccessToken
+    
+    Personal access token used to authenticate that has been converted to a secure string. 
+    It is recomended to uses an Azure Pipelines PS session to pass the personal access token parameter among funcitons, See New-APSession.
+    https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts
+    
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to send the request.
+
+    .PARAMETER Proxy
+    
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+    
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    Azure DevOps PS session, created by New-APSession.
+
+    .PARAMETER StorageKey
+
+    Storage key of the subject (user, group, scope, etc.) to resolve.
+
+    .INPUTS
+    
+    None, does not support the pipeline.
+
+    .OUTPUTS
+
+    PSObject, Azure Pipelines account(s)
+
+    .EXAMPLE
+
+    Returns a subject descriptor of a Graph entity.
+
+    Get-APDescriptor -Instance 'https://dev.azure.com' -Collection 'myCollection' -ApiVersion 5.0-preview -StorageKey '16c8b591-362d-4d7f-b77b-b04c2f110c52'
+
+    .LINK
+
+    https://docs.microsoft.com/en-us/rest/api/azure/devops/graph/descriptors/get?view=azure-devops-rest-5.0
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByPersonalAccessToken')]
+    Param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Instance,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Collection,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $ApiVersion,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Security.SecureString]
+        $PersonalAccessToken,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [string]
+        $StorageKey
+    )
+
+    begin
+    {
+        If ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-APSession
+            If ($currentSession)
+            {
+                $Instance = $currentSession.Instance
+                $Collection = $currentSession.Collection
+                $PersonalAccessToken = $currentSession.PersonalAccessToken
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
+            }
+        }
+    }
+    
+    process
+    {
+        If ($ApiVersion -notmatch '5.*')
+        {
+            Write-Error "[$($MyInvocation.MyCommand.Name)]: Groups are not supported in api versions earlier the 5.0." -ErrorAction 'Stop'
+        }
+        $apiEndpoint = (Get-APApiEndpoint -ApiType 'graph-descriptorStorageKey') -f $StorageKey
+        $queryParameters = Set-APQueryParameters -InputObject $PSBoundParameters
+        $setAPUriSplat = @{
+            Collection  = $Collection
+            Instance    = $Instance
+            ApiVersion  = $ApiVersion
+            ApiEndpoint = $apiEndpoint
+            Query       = $queryParameters
+        }
+        [uri] $uri = Set-APUri @setAPUriSplat
+        $invokeAPRestMethodSplat = @{
+            Method              = 'GET'
+            Uri                 = $uri
+            Credential          = $Credential
+            PersonalAccessToken = $PersonalAccessToken
+            Proxy               = $Proxy
+            ProxyCredential     = $ProxyCredential
+        }
+        $results = Invoke-APRestMethod @invokeAPRestMethodSplat 
+        If ($results.value)
+        {
+            return $results.value
+        }
+        else
+        {
+            return $results
+        }
+    }
+    
+    end
+    {
+    }
+}
