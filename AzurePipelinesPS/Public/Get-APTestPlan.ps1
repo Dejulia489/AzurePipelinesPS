75cee7fbca34be7cc47d613ed959d64e43cbed32
mdejulia <Michael.DeJulia@PacificLife.com>
2020-08-14 16:18:22 -0400

    Get test plan

---
+++ AzurePipelinesPS/Public/Get-APTestPlan.ps1
@@ -0,0 +1,194 @@
+function Get-APTestPlan
+{
+    <#
+    .SYNOPSIS
+
+    Returns an Azure Pipeline test plan.
+
+    .DESCRIPTION
+
+    Returns an Azure Pipeline test plan based on a plan id. 
+    The plan id can be returned with Get-APTestPlanList.
+
+    .PARAMETER Instance
+    
+    The Team Services account or TFS server.
+    
+    .PARAMETER Collection
+    
+    For Azure DevOps the value for collection should be the name of your orginization. 
+    For both Team Services and TFS The value should be DefaultCollection unless another collection has been created.
+
+    .PARAMETER Project
+    
+    Project ID or project name.
+
+    .PARAMETER ApiVersion
+    
+    Version of the api to use.
+
+    .PARAMETER PersonalAccessToken
+    
+    Personal access token used to authenticate that has been converted to a secure string. 
+    It is recomended to uses an Azure Pipelines PS session to pass the personal access token parameter among funcitons, See New-APSession.
+    https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts
+    
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to send the request.
+
+    .PARAMETER Proxy
+    
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+    
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    Azure DevOps PS session, created by New-APSession.
+
+    .PARAMETER PlanId
+
+    Id of the test plan to get.
+
+    .INPUTS
+    
+    None, does not support pipeline.
+
+    .OUTPUTS
+
+    PSObject, Azure Pipelines test plan(s)
+
+    .EXAMPLE
+
+    Returns AP test plan list for 'myFirstProject' and the plan id of '8'.
+
+    Get-APTestPlan -Instance 'https://dev.azure.com' -Collection 'myCollection' -Project 'myFirstProject' -PlanId 8
+
+    .LINK
+
+    https://docs.microsoft.com/en-us/rest/api/azure/devops/testplan/test%20%20plans/get?view=azure-devops-rest-5.1
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByPersonalAccessToken')]
+    Param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Instance,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Collection,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Project,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $ApiVersion,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Security.SecureString]
+        $PersonalAccessToken,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [int]
+        $PlanId
+    )
+
+    begin
+    {
+        If ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-APSession
+            If ($currentSession)
+            {
+                $Instance = $currentSession.Instance
+                $Collection = $currentSession.Collection
+                $Project = $currentSession.Project
+                $PersonalAccessToken = $currentSession.PersonalAccessToken
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
+            }
+        }
+    }
+    
+    process
+    {
+        $apiEndpoint = (Get-APApiEndpoint -ApiType 'test-testPlanId') -f $PlanId
+        $queryParameters = Set-APQueryParameters -InputObject $PSBoundParameters
+        $setAPUriSplat = @{
+            Collection  = $Collection
+            Instance    = $Instance
+            Project     = $Project
+            ApiVersion  = $ApiVersion
+            ApiEndpoint = $apiEndpoint
+            Query       = $queryParameters
+        }
+        [uri] $uri = Set-APUri @setAPUriSplat
+        $invokeAPRestMethodSplat = @{
+            Method              = 'GET'
+            Uri                 = $uri
+            Credential          = $Credential
+            PersonalAccessToken = $PersonalAccessToken
+            Proxy               = $Proxy
+            ProxyCredential     = $ProxyCredential
+        }
+        $plans = Invoke-APRestMethod @invokeAPRestMethodSplat 
+        If ($plans.value)
+        {
+            return $plans.value
+        }
+        else
+        {
+            return $plans
+        }
+    }
+    
+    end
+    {
+    }
+}
