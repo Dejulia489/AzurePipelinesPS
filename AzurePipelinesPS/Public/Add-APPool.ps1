688b0b3b56ff34d2255904e4fc80f886e965a106
Michael Dejulia <michael.dejulia@gmail.com>
2021-03-17 14:46:14 -0400

    Add-APPool, Add-APQueue

---
+++ AzurePipelinesPS/Public/Add-APPool.ps1
@@ -0,0 +1,217 @@
+function Add-APPool
+{
+    <#
+    .SYNOPSIS
+
+    Creates an Azure Pipeline agent pool.
+
+    .DESCRIPTION
+
+    Creates an Azure Pipeline agent pool.
+
+    .PARAMETER Instance
+    
+    The Team Services account or TFS server.
+    
+    .PARAMETER Collection
+    
+    For Azure DevOps the value for collection should be the name of your orginization. 
+    For both Team Services and TFS The value should be DefaultCollection unless another collection has been created.
+
+    .PARAMETER ApiVersion
+    
+    Version of the api to use.
+
+    .PARAMETER PersonalAccessToken
+    
+    Personal access token used to authenticate that has been converted to a secure string. 
+    It is recomended to uses an Azure Pipelines PS session to pass the personal access token parameter among funcitons, See New-APSession.
+    https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts
+
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to send the request.
+    
+    .PARAMETER Proxy
+    
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+    
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    Azure DevOps PS session, created by New-APSession.
+
+    .PARAMETER Name
+    
+    Sets name of the variable group.
+
+    .PARAMETER PoolType
+
+    The type of pool, automation or deployment.
+
+    .PARAMETER AutoProvision
+
+    Should the pool be provisioned in all the projects in the collection.
+
+    .PARAMETER Scope
+
+    The scope of the pool. GUID of the project collection.
+
+    .INPUTS
+
+    None, does not support pipeline.
+
+    .OUTPUTS
+
+    PSObject, Azure Pipelines variable group.
+
+    .EXAMPLE
+
+    Adds a pool named 'myNewPool'
+
+    Add-APPool -Session 'mySession' -Name 'myNewPool'
+
+    .LINK
+
+    https://docs.microsoft.com/en-us/rest/api/azure/devops/distributedtask/pools/add?view=azure-devops-rest-6.1
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByPersonalAccessToken')]
+    Param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Instance,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Collection,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $ApiVersion,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Security.SecureString]
+        $PersonalAccessToken,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [object]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [string]
+        $Name, 
+
+        [Parameter()]
+        [ValidateSet('automation', 'deployment')]
+        [string]
+        $PoolType, 
+
+        [Parameter()]
+        [bool]
+        $AutoProvision = $true, 
+
+        [Parameter()]
+        [string]
+        $Scope
+    )
+
+    begin
+    {
+        If ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-APSession
+            If ($currentSession)
+            {
+                $Instance = $currentSession.Instance
+                $Collection = $currentSession.Collection
+                $PersonalAccessToken = $currentSession.PersonalAccessToken
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
+            }
+        }
+    }
+    
+    process
+    {
+        $body = @{
+            name          = $Name
+            autoProvision = $AutoProvision
+        }
+        If ($PoolType)
+        {
+            $body.poolType = $PoolType
+        }
+        If ($Scope)
+        {
+            $body.scope = $Scope
+        }
+        $apiEndpoint = Get-APApiEndpoint -ApiType 'pool-pools'
+        $setAPUriSplat = @{
+            Collection  = $Collection
+            Instance    = $Instance
+            ApiVersion  = $ApiVersion
+            ApiEndpoint = $apiEndpoint
+        }
+        [uri] $uri = Set-APUri @setAPUriSplat
+        $invokeAPRestMethodSplat = @{
+            Method              = 'POST'
+            Uri                 = $uri
+            Credential          = $Credential
+            PersonalAccessToken = $PersonalAccessToken
+            Body                = $body
+            ContentType         = 'application/json'
+            Proxy               = $Proxy
+            ProxyCredential     = $ProxyCredential
+        }
+        $results = Invoke-APRestMethod @invokeAPRestMethodSplat 
+        If ($results.value)
+        {
+            return $results.value
+        }
+        else
+        {
+            return $results
+        }
+    }
+    
+    end
+    {
+    }
+}
