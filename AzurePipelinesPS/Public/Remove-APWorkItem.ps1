d1a510f721e153326eaea28ad1baad0e8ef00b97
Michael Dejulia <Michael.Dejulia@gmail.com>
2024-05-15 09:07:20 -0400

    Remove-APWorkItem

---
+++ AzurePipelinesPS/Public/Remove-APWorkItem.ps1
@@ -0,0 +1,189 @@
+function Remove-APWorkItem
+{
+    <#
+    .SYNOPSIS
+
+    Deletes an Azure Pipeline work item.
+
+    .DESCRIPTION
+
+    Deletes an Azure Pipeline work item.
+    The id can be retrieved by using Get-APWorkItemList.
+
+    .PARAMETER Instance
+
+    The Team Services account or TFS server.
+    For Azure DevOps the value should be https://dev.azure.com/.
+
+    .PARAMETER Collection
+
+    For Azure DevOps the value for collection should be the name of your orginization.
+    For both Team Services and TFS The value should be DefaultCollection unless another collection has been created.
+
+    .PARAMETER ApiVersion
+
+    Version of the api to use.
+
+    .PARAMETER PersonalAccessToken
+
+    Personal access token used to authenticate that has been converted to a secure string.
+    It is recomended to uses an Azure Pipelines PS session to pass the personal access token parameter among funcitons, See New-APSession.
+    https://docs.microsoft.com/en-us/azure/devops/organizations/accounts/use-personal-access-tokens-to-authenticate?view=vsts
+
+    .PARAMETER Credential
+
+    Specifies a user account that has permission to send the request.
+
+    .PARAMETER Proxy
+
+    Use a proxy server for the request, rather than connecting directly to the Internet resource. Enter the URI of a network proxy server.
+
+    .PARAMETER ProxyCredential
+
+    Specifie a user account that has permission to use the proxy server that is specified by the -Proxy parameter. The default is the current user.
+
+    .PARAMETER Session
+
+    Azure DevOps PS session, created by New-APSession.
+
+    .PARAMETER Id
+
+    The id of the work item to be deleted.
+
+    .PARAMETER Destory
+
+    Flag for permanently destroying the work item.
+
+    .INPUTS
+
+    None, does not support pipeline.
+
+    .OUTPUTS
+
+    None, does not support output.
+
+    .EXAMPLE
+
+    Deletes work item with the id of '5'.
+
+    Remove-APWorkItem -Instance 'https://dev.azure.com' -Collection 'myCollection' -Project 'myFirstProject' -FeedId 5
+
+    .LINK
+
+    https://learn.microsoft.com/en-us/rest/api/azure/devops/wit/work-items/delete?view=azure-devops-rest-7.1&tabs=HTTP
+    #>
+    [CmdletBinding(DefaultParameterSetName = 'ByPersonalAccessToken')]
+    Param
+    (
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [uri]
+        $Instance,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $Collection,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(Mandatory,
+            ParameterSetName = 'ByCredential')]
+        [string]
+        $ApiVersion,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Security.SecureString]
+        $PersonalAccessToken,
+
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $Credential,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [string]
+        $Proxy,
+
+        [Parameter(ParameterSetName = 'ByPersonalAccessToken')]
+        [Parameter(ParameterSetName = 'ByCredential')]
+        [pscredential]
+        $ProxyCredential,
+
+        [Parameter(Mandatory,
+            ParameterSetName = 'BySession')]
+        [ArgumentCompleter( {
+                param ( $commandName,
+                    $parameterName,
+                    $wordToComplete,
+                    $commandAst,
+                    $fakeBoundParameters )
+                $possibleValues = Get-APSession | Select-Object -ExpandProperty SessionNAme
+                $possibleValues.Where( { $PSitem -match $wordToComplete })
+            })]
+        [object]
+        $Session,
+
+        [Parameter(Mandatory)]
+        [string[]]
+        $Id,
+
+        [Parameter()]
+        [bool]
+        $Destory
+    )
+
+    begin
+    {
+        If ($PSCmdlet.ParameterSetName -eq 'BySession')
+        {
+            $currentSession = $Session | Get-APSession
+            If ($currentSession)
+            {
+                $Instance = $currentSession.Instance
+                $Collection = $currentSession.Collection
+                $PersonalAccessToken = $currentSession.PersonalAccessToken
+                $Credential = $currentSession.Credential
+                $Proxy = $currentSession.Proxy
+                $ProxyCredential = $currentSession.ProxyCredential
+                If ($currentSession.Version)
+                {
+                    $ApiVersion = (Get-APApiVersion -Version $currentSession.Version)
+                }
+                else
+                {
+                    $ApiVersion = $currentSession.ApiVersion
+                }
+            }
+        }
+    }
+
+    process
+    {
+        $apiEndpoint = (Get-APApiEndpoint -ApiType 'wit-workitemId') -f $Id
+        $setAPUriSplat = @{
+            Collection  = $Collection
+            Instance    = $Instance
+            ApiVersion  = $ApiVersion
+            ApiEndpoint = $apiEndpoint
+        }
+        [uri] $uri = Set-APUri @setAPUriSplat
+        $invokeAPRestMethodSplat = @{
+            Method              = 'DELETE'
+            Uri                 = $uri
+            Credential          = $Credential
+            PersonalAccessToken = $PersonalAccessToken
+            Proxy               = $Proxy
+            ProxyCredential     = $ProxyCredential
+        }
+        Invoke-APRestMethod @invokeAPRestMethodSplat
+    }
+
+    end
+    {
+    }
+}
